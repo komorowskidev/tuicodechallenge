aed766baa947b41c1f6a6fd80c1a3c3d2cc13d9c
komorowskidev <komorowskidev@gmail.com>
2023-09-30 21:53:03 +0200

    initial commit

---
+++ src/main/kotlin/com/komorowskidev/tuicodechallenge/TuiCodeChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.komorowskidev.tuicodechallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TuiCodeChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<TuiCodeChallengeApplication>(*args)
+}
